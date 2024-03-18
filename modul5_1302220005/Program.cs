738a7bb8662176a2a7827ececd1d1bbb6fd4f41e
wildanniam <wildanniam4@gmail.com>
2024-03-18 07:03:33 +0700

    menambahkan function jumlahTigaAngka

--- modul5_1302220005/Program.cs
+++ modul5_1302220005/Program.cs
@@ -1,2 +1,20 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+public class Penjumlahan
+{
+    public double JumlahTigaAngka<T>(T a, T b, T c)
+    {
+        dynamic aa = a;
+        dynamic bb = b;
+        dynamic cc = c;
+        return aa + bb + cc;
+    }
+}
+public class Program
+{
+    //NIM: 1302220005
+    public static void Main(string[]args)
+    {
+        Penjumlahan penjumlahan = new Penjumlahan();
+        Console.WriteLine(penjumlahan.JumlahTigaAngka(13, 02, 22)); 
+    }
+}
